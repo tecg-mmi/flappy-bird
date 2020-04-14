c0e7c1d7dc6c96022eb03381f7f19da102218c2a
Dominique Vilain <dominique.vilain@hepl.be>
2020-04-14 22:45:24 +0200

    add birdie base animation

--- src/js/birdie.js
+++ src/js/birdie.js
@@ -7,6 +7,8 @@ const birdie = {
     { sx: 62, sy: 982 },
     { sx: 118, sy: 982 }
   ],
+  maxAnimationStep: 2,
+  animationStep: 0,
   width: 34,
   height: 24,
   x: 0,
@@ -25,13 +27,15 @@ const birdie = {
   },
 
   render () {
+    this.animationStep = this.animationStep < this.maxAnimationStep ? this.animationStep + 1 : 0
+    console.log(this.animationStep)
     this.game.context.save()
     this.game.context.translate(this.x, this.y)
     this.game.context.rotate(0)
     this.game.renderSpriteFrame(
       {
-        sx: this.frames[0].sx,
-        sy: this.frames[0].sy,
+        sx: this.frames[this.animationStep].sx,
+        sy: this.frames[this.animationStep].sy,
         sw: this.width,
         sh: this.height,
         dx: -this.width / 2,
